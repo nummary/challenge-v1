734f61897a89809ce6c4860a51ff10bc71ac9d63
nummary <9779092199r@gmail.com>
2026-08-24 02:01:18 +0300

    Сделал корректный вывод фотографий при создании темы

--- nummary/create_thread.php
+++ nummary/create_thread.php
@@ -5,14 +5,14 @@ require_once "db.php";
 if (isset($_SESSION['uid'])) {
 
 	if (isset($_GET['section_id'])) {
-	    // Превращаем входящую строку строго в целое число
 	    $section_id = (int)$_GET['section_id']; 
 	} else {
 	    echo "Упс... Вы не туда попали" . '</br>';
 	    ?>
 	    <a href="javascript:history.back()"><button>Назад</button></a><br>
 	    <a href="index.php"><button>На главную</button></a><br>
-	    <?php 
+	    <?php
+	    exit();
 	}
 
 	if ($_SERVER['REQUEST_METHOD'] === "POST") {
@@ -37,49 +37,46 @@ if (isset($_SESSION['uid'])) {
 			'uid' => $_SESSION['uid'],
 			'text' => $thread_text
 		]);
+		$last_comment_id = $pdo->lastInsertId();
+
+		if (!empty($_FILES['comment_images']['name'][0])) {
+			$uploadDir = 'uploads/comments/';
+
+			$ImgStmt = $pdo->prepare("INSERT INTO `comment_images` (`cid`, `path`, `original_name`) VALUES (:lastcid, :path, :orig_name)");
+
+			foreach ($_FILES['comment_images']['tmp_name'] as $key => $tmpName) {
+				if ($_FILES['comment_images']['error'][$key] === UPLOAD_ERR_OK) {
+					$fileName = $_FILES['comment_images']['name'][$key];
+					$origName = $_FILES['comment_images']['name'][$key];
+					$ext = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+
+					$newName = uniqid('file_', true) . "." . $ext;
+					$destination = $uploadDir . $newName;
+
+					if (move_uploaded_file($tmpName, $destination)) {
+						$ImgStmt->execute([
+							'lastcid' => $last_comment_id,
+							'path' => $destination,
+							'orig_name' => $origName
+						]);
+					}
+				}
+			}
+			header("Location: thread.php?id={$last_tread}");
+			exit();
+		}
 		header("Location: thread.php?id={$last_tread}");
 		exit();
 	}
 
 
+} else {
 
+	header("Location: index.php");
+	exit();
 
-} else { ?>
-	<a href="register.html"><button>Регистрация</button></a><br>
-    <a href="login.html"><button>Войти</button></a>
-<?php } ?>
+} ?>
 
-<!-- <?php $stmt = $pdo->query("SELECT 
-t.`tid`, 
-t.`name`, 
-t.`created_dt`, 
-u_author.`username` AS `author_name`,
-COUNT(c.`cid`) AS `comments_count`,
-MAX(c.`created_dt`) AS `last_comment_dt`,
-u_last.`username` AS `last_commenter_name`
-FROM `threads` t
-LEFT JOIN `users` u_author ON t.`uid` = u_author.`uid`
-LEFT JOIN `comments` c ON t.`tid` = c.`tid`
-LEFT JOIN `comments` c_last ON c_last.`cid` = (
-SELECT MAX(`cid`) FROM `comments` WHERE `tid` = t.`tid`
-)
-LEFT JOIN `users` u_last ON c_last.`uid` = u_last.`uid`
-WHERE t.`sid` = $section_id
-GROUP BY t.`tid`");
-$thrds = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-foreach ($thrds as $row) {
-    // Если комментариев еще нет, пишем 'Нет ответов'
-    $last_user = $row['last_commenter_name'] ? htmlspecialchars($row['last_commenter_name']) : 'Нет ответов';
-    $last_time = $row['last_comment_dt'] ? $row['last_comment_dt'] : '-';
-
-    echo "<a href='thread.php?id=" . $row['tid'] . "'>" . htmlspecialchars($row['name']) . "</a>";
-    echo " | Автор: " . htmlspecialchars($row['author_name']);
-    echo " | Ответов: " . $row['comments_count'];
-    echo " | Последнее от: " . $last_user . " (" . $last_time . ")<br>";
-}
-
-?> -->
 
 <!DOCTYPE html>
 <html lang="ru">
@@ -92,13 +89,13 @@ foreach ($thrds as $row) {
     <h1>Форум</h1>
     <h2>Создать тему</h2>
 
-    <form action="create_thread.php?section_id=<?php echo $section_id ?>" method="POST">
+    <form action="create_thread.php?section_id=<?php echo $section_id ?>" method="POST" enctype="multipart/form-data">
         <h2>Заголовок темы</h2>
         <input type="text" name="thread_name" placeholder="Заголовок темы" required="true">
         <h2>Текст темы</h2>
         <textarea name="thread_text" rows="5" cols="60" placeholder="Текст темы..."></textarea><br><br>
 		<label>Прикрепить файлы:</label><br>
-        <input type="file" name="comment_images[]" multiple><br><br>
+        <input type="file" name="comment_images[]" multiple ><br><br>
         <button type="submit" name="send_comment">Создать тему</button>
     </form>
         
